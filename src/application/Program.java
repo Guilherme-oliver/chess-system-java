31a95ed1de6968c00f12a359a0f7a2319d911d68
Guilherme Oliveira <guilherme.rodriguess.oliveira@gmail.com>
2022-01-11 14:30:44 -0300

    Up-date

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import boardgame.Position;
+
+import java.awt.*;
+
 public class Program {
     public static void main(String[] args) {
-        
-        System.out.println("Hollo World");
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
