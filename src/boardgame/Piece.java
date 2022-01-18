39afdbb66bb6fb080b381066ed5f784b05ba0414
Guilherme Oliveira <guilherme.rodriguess.oliveira@gmail.com>
2022-01-18 07:27:16 -0300

    fixing some functionality bugs

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,35 +2,34 @@ package boardgame;
 
 public abstract class Piece {
 
-    protected Position position;
-    private Board board;
+        protected Position position;
+        private Board board;
 
-    public Piece(Board board) {
-        this.board = board;
-        position = null;
-    }
-
-    protected Board getBoard() {
-        return board;
-    }
-
-    public abstract boolean[][] possibleMoves();
-
-    public boolean possibleMove(Position position) {
-        return possibleMoves()[position.getRow()][position.getColumn()];
-        //Um metodo concreto que esta utilizando um metodo abstrato ou metodo "hook"
-    }
-
-    public boolean isThereAnyPossibleMove() {
-        boolean[][] mat = possibleMoves();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
-                if (mat[i][j]){
-                return true;
-            }
+        public Piece(Board board) {
+            this.board = board;
+            position = null;
         }
-    }
-    return false;
+        protected Board getBoard() {
+            return board;
+        }
 
-    }
+        public abstract boolean[][] possibleMoves();
+
+        public boolean possibleMove(Position position) {
+            return possibleMoves()[position.getRow()][position.getColumn()];
+        }
+
+        public boolean isThereAnyPossibleMove() {
+            boolean[][] mat = possibleMoves();
+            for (int i=0; i<mat.length; i++) {
+                for (int j=0; j<mat.length; j++) {
+                    if (mat[i][j]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 }
+
+
