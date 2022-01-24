56e226299be89b807236aa1a097e3a4a27ff8885
Guilherme Oliveira <guilherme.rodriguess.oliveira@gmail.com>
2022-01-24 10:05:28 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,11 +1,8 @@
 package chess;
-
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
-
 public abstract class ChessPiece extends Piece {
-
     private Color color;
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -13,6 +10,10 @@ public abstract class ChessPiece extends Piece {
     }
     public Color getColor() {
         return color;
+    }
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
     }
 
     protected boolean isThereOpponentPiece(Position position) {
